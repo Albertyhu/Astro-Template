383906a3d2520e39f9086ae8e6b312c0aac70d6f
Albertyhu <hualbert@rocketmail.com>
2023-07-24 17:36:57 -0700

    Added Netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import myAdapter from '@astrojs/my-adapter';
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [preact(), react(), tailwind()],
+  adapter: netlify(), 
 });
