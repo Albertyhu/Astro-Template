24d22e3b357ed9ea3741d170854c606e7256c78f
Albertyhu <hualbert@rocketmail.com>
2023-07-24 17:42:08 -0700

    saving latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import myAdapter from '@astrojs/my-adapter';
-
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
