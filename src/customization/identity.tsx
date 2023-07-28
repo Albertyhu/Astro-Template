cbe28d2cde5ea2a5ce5bc14f6a5aeaa0e8ff335d
Albertyhu <hualbert@rocketmail.com>
2023-07-27 21:04:00 -0700

    added dynamic metatags

--- src/customization/identity.tsx
+++
@@ -1,5 +0,0 @@
-export const WebsiteName = "BusinessName"
-export const WebsiteTitle = "Business Name"
-export const OwnerName = "TheLastSliceOfPizza"
-export const OwnerEmail = "hualbert@rocketmail.com"
-export const YEAR = new Date().getFullYear(); 
