0903194b2d897770bd0b117f3f67d30b8e12145d
Albertyhu <hualbert@rocketmail.com>
2023-07-28 17:16:03 -0700

    saving latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
   },
   output: 'server',
   integrations: [preact(), react(), tailwind(), sitemap()],
-  //adapter: netlify(), 
+  adapter: netlify(), 
   adapter: node({
     mode: "standalone"
   })
