5104ef90746c827506543ea7f1aee574f8f79242
Albertyhu <hualbert@rocketmail.com>
2023-07-29 18:07:08 -0700

    saving latest changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,70 @@ export default defineConfig({
     react(), 
     tailwind(), 
     sitemap(),
+    NetlifyCMS({
+    config: {
+      backend: {
+        name: 'git-gateway',
+        branch: 'main',
+      },
+      publish_mode: "editorial_workflow",
+      media_folder: "src/assets",
+      public_folder: "/assets/uploads", 
+      collections: [{
+        name: "posts",
+        label: "Blog Posts",
+        folder: "src/pages/posts",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "layout",
+          widget: "hidden",
+          label: "Layout",
+          default: '../../layouts/MarkdownPostLayout.astro',
+
+        }, {
+          name: "title",
+          widget: 'string',
+          label: "Title"
+        }, {
+          name: "pubDate",
+          widget: 'datetime',
+          label: "Published date"
+        }, {
+          name: "description",
+          widget: 'string',
+          label: "Description"
+        }, {
+          name: "author",
+          widget: 'string',
+          label: "Author"
+        }, {
+          name: "images",
+          widget: 'list',
+          label: "Images",
+          fields: [{
+            name: "image",
+            widget: 'image',
+            label: "Image"
+          }]
+        }, {
+          name: "tags",
+          widget: 'list',
+          label: "tags",
+          fields:[{
+            name: "tag",
+            widget: "string",
+            label: "Tag",             
+          }]
+        }, {
+          name: "body",
+          widget: 'markdown',
+          label: "Body"
+        },
+      ]
+      }],
+    }
+  }), 
   ],
   adapter: netlify(), 
   // adapter: node({
