ea3950a5a98fe10afd09052ef119718ad464a0e6
Albertyhu <hualbert@rocketmail.com>
2023-07-31 21:20:43 -0700

    back to experimental

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: false
+    assets: true
   },
   output: 'server',
   integrations: [
